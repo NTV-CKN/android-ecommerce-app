e9b29c9cdf9e1786836e086e5c88ed6e7e780c91
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-06-12 12:35:00 +0700

    Thiết lập drawer và Navigation View cho quản lí tài khoản người dùng

---
+++ app/src/main/java/com/infix/phukiencongnghe/ui/user_manage/UserManagerActivity.java
@@ -0,0 +1,64 @@
+package com.infix.phukiencongnghe.ui.user_manage;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.navigation.NavigationView;
+import com.infix.phukiencongnghe.R;
+import com.infix.phukiencongnghe.databinding.ActivityUserManagerBinding;
+
+import java.util.Objects;
+
+public class UserManagerActivity extends AppCompatActivity {
+    private ActivityUserManagerBinding binding;
+    private ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityUserManagerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setupMyToolbar();
+    }
+
+    private void setupMyToolbar() {
+        setSupportActionBar(binding.toolbarUserManage);
+
+        toggle = new ActionBarDrawerToggle(
+                this,
+                binding.drawerLayout,
+                binding.toolbarUserManage,
+                R.string.open,
+                R.string.close
+        );
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        binding.navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_home) {
+
+                } else if (id == R.id.nav_address_user) {
+
+                } else if (id == R.id.nav_logout) {
+
+                }
+
+                binding.drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+    }
+}
