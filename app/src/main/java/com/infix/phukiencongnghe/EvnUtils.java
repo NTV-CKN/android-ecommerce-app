9711ac81eea6ebb13c9d04ece78ef5a64279f4fa
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-06-12 22:29:34 +0700

    Chỉnh sửa lại giao diện main activity, tạo HomeFragment và đổi tên MainViewModel thành HomeViewModel, cập nhật lại gitignore

--- app/src/main/java/com/infix/phukiencongnghe/EvnUtils.java
+++
@@ -1,5 +0,0 @@
-package com.infix.phukiencongnghe;
-
-public class EvnUtils {
-    public static final String BASE_URL = "http://172.20.10.4:8080";
-}
--- app/src/main/java/com/infix/phukiencongnghe/ui/main/MainActivity.java
+++ app/src/main/java/com/infix/phukiencongnghe/ui/main/MainActivity.java
@@ -3,85 +3,25 @@ package com.infix.phukiencongnghe.ui.main;
 import static com.infix.phukiencongnghe.R.id.main;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.infix.phukiencongnghe.R;
-import com.infix.phukiencongnghe.data.repository.main.category.CategoryRepositoryImpl;
-import com.infix.phukiencongnghe.data.repository.main.product.FeatureProductRepositoryImpl;
-import com.infix.phukiencongnghe.ui.adapter.categories.CategoryAdapter;
-import com.infix.phukiencongnghe.ui.adapter.feature_product.FeatureProductAdapter;
-
-import java.util.ArrayList;
+import com.infix.phukiencongnghe.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private RecyclerView recycleViewCategory;
-    private RecyclerView recyclerViewProduct;
-    private MainViewModel mainViewModel;
-    private FeatureProductAdapter featureProductAdapter;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        recycleViewCategory = findViewById(R.id.recycleView_category);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recycleViewCategory.setLayoutManager(layoutManager);
-
-        recyclerViewProduct = findViewById(R.id.recycleView_product);
-        GridLayoutManager productManager = new GridLayoutManager(this, 2) {
-            @Override
-            public boolean canScrollVertically() {
-                return false;
-            }
-        };
-        recyclerViewProduct.setLayoutManager(productManager);
-        recyclerViewProduct.setNestedScrollingEnabled(false);
-
-
-        featureProductAdapter = new FeatureProductAdapter(new ArrayList<>());
-        recyclerViewProduct.setAdapter(featureProductAdapter);
-
-        MainViewModel.Factory factory = new MainViewModel.Factory(
-                new CategoryRepositoryImpl(),
-                new FeatureProductRepositoryImpl()
-        );
-        mainViewModel = new ViewModelProvider(this, factory).get(MainViewModel.class);
-
-        mainViewModel.categoryLiveData.observe(this, categoryList -> {
-            if(categoryList != null) {
-                CategoryAdapter adapter = new CategoryAdapter(categoryList);
-                recycleViewCategory.setAdapter(adapter);
-            }
-        });
-
-        mainViewModel.ftProdLiveData.observe(this, featureProductList -> {
-            if(featureProductList != null) {
-                featureProductAdapter.setData(featureProductList);
-            }
-        });
-
-        mainViewModel.notifyMsg.observe(this, errorMsg -> {
-            if(errorMsg != null) {
-                Toast.makeText(this, errorMsg, Toast.LENGTH_SHORT).show();
-                mainViewModel.resetStates();
-            }
-        });
-
-        mainViewModel.loadParentCategories();
-        mainViewModel.loadFeatureProduct(0, 22);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
