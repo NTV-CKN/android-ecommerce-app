5df1e8bc44987926b6bea0a9af7bd5c2b469185a
NTT-CKN <tintaptucteo@gmail.com>
2026-06-25 03:42:22 +0700

    test

--- app/src/main/java/com/infix/phukiencongnghe/ui/auth/AuthActivity.java
+++ app/src/main/java/com/infix/phukiencongnghe/ui/auth/AuthActivity.java
@@ -1,6 +1,7 @@
 package com.infix.phukiencongnghe.ui.auth;
 
 import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,7 +10,6 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.infix.phukiencongnghe.R;
 import com.infix.phukiencongnghe.databinding.ActivityAuthBinding;
-import com.infix.phukiencongnghe.ui.adapter.feature_product.ProductDetailsFragment;
 
 public class AuthActivity extends AppCompatActivity {
     private ActivityAuthBinding binding;
@@ -18,20 +18,11 @@ public class AuthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding = ActivityAuthBinding.inflate(getLayoutInflater());
-
-        setContentView(binding.getRoot());
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.main, (v, insets) -> {
+        setContentView(R.layout.activity_auth);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.main, new ProductDetailsFragment())
-                    .commit();
-        }
     }
 }
