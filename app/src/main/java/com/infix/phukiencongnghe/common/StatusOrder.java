7c5412f7721ec8c6713e557a5bea742975ab5cb6
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-06-28 21:14:09 +0700

    thêm StatusOrder enum

---
+++ app/src/main/java/com/infix/phukiencongnghe/common/StatusOrder.java
@@ -0,0 +1,15 @@
+package com.infix.phukiencongnghe.common;
+
+public enum StatusOrder {
+    PENDING_APPROVE("pending_approve"),
+    DELIVERING("delivering"),
+    COMPLETED("completed"),
+    CANCEL("cancel"),
+    APPROVE_BY_ADMIN("approve_by_admin"),
+    REJECT_BY_ADMIN("reject_by_admin");
+
+    String value;
+     StatusOrder(String value) {
+        this.value = value;
+    }
+}
