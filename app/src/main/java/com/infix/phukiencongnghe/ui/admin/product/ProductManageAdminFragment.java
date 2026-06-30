a68f77ac2e98bac4346a317342042dbc80a1760d
NguyenTruongVu <nguyentruongvu000111@gmail.com>
2026-06-30 17:13:44 +0700

    Hiện các thể loại cho ProductManageAdminFragment

--- app/src/main/java/com/infix/phukiencongnghe/ui/admin/product/ProductManageAdminFragment.java
+++ app/src/main/java/com/infix/phukiencongnghe/ui/admin/product/ProductManageAdminFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +13,17 @@ import android.view.ViewGroup;
 
 import com.infix.phukiencongnghe.R;
 import com.infix.phukiencongnghe.databinding.FragmentProductManageAdminBinding;
+import com.infix.phukiencongnghe.ui.adapter.categories.CategoryAdapter;
+import com.infix.phukiencongnghe.utils.InjectUtils;
+
+import java.util.ArrayList;
 
 public class ProductManageAdminFragment extends Fragment {
     private FragmentProductManageAdminBinding binding;
+
+    private ProductManageAdminViewModel productManageAdminViewModel;
+
+    private CategoryAdapter categoryAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +39,38 @@ public class ProductManageAdminFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initRvCategory();
+        initAndObserveProductManageAdminVM();
+    }
+
+    private void initRvCategory() {
+        categoryAdapter = new CategoryAdapter(new ArrayList<>(), category -> {
+
+        });
+
+        binding.rvCategory.setAdapter(categoryAdapter);
+    }
+
+    private void initAndObserveProductManageAdminVM() {
+        //init view model
+        ProductManageAdminViewModel.Factory factory =
+                new ProductManageAdminViewModel.Factory(
+                        InjectUtils.createCategoryRepository(),
+                        InjectUtils.createProductAdminRepository()
+                );
+
+        productManageAdminViewModel = new ViewModelProvider(requireActivity(), factory)
+                .get(ProductManageAdminViewModel.class);
+
+        //observe categories
+        productManageAdminViewModel.loadCategoriesAvailable();
+        productManageAdminViewModel.categories.observe(
+                getViewLifecycleOwner(),
+                categoryDTOS -> {
+                    if(categoryDTOS == null) return;
+
+                    categoryAdapter.update(categoryDTOS);
+        });
 
     }
 }
--- app/src/main/java/com/infix/phukiencongnghe/ui/admin/product/ProductManageAdminViewModel.java
+++ app/src/main/java/com/infix/phukiencongnghe/ui/admin/product/ProductManageAdminViewModel.java
@@ -1,8 +1,12 @@
 package com.infix.phukiencongnghe.ui.admin.product;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.infix.phukiencongnghe.data.dto.ProductAdminPageDTO;
 import com.infix.phukiencongnghe.data.dto.response.CategoryDTO;
@@ -12,6 +16,10 @@ import com.infix.phukiencongnghe.data.repository.common.category.ICategoryReposi
 import com.infix.phukiencongnghe.utils.paging.PaginationManager;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProductManageAdminViewModel extends ViewModel {
     private final ICategoryRepository categoryRepository;
@@ -31,5 +39,43 @@ public class ProductManageAdminViewModel extends ViewModel {
         this.categoryRepository = categoryRepository;
         this.productAdminRepository = productAdminRepository;
         paginationManager = new PaginationManager(12);
+    }
+
+    public void loadCategoriesAvailable() {
+        categoryRepository.getParentCategory().enqueue(new Callback<List<CategoryDTO>>() {
+            @Override
+            public void onResponse(Call<List<CategoryDTO>> call, Response<List<CategoryDTO>> response) {
+                if (response.isSuccessful())
+                    if (response.body() != null)
+                        _categories.setValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<CategoryDTO>> call, Throwable throwable) {
+                Log.d("ProductManageAdminViewModel", throwable.getMessage());
+            }
+        });
+    }
+
+    public static class Factory implements ViewModelProvider.Factory {
+
+        private final ICategoryRepository categoryRepository;
+        private final IProductAdminRepository productAdminRepository;
+
+        public Factory(ICategoryRepository categoryRepository,
+                       IProductAdminRepository productAdminRepository) {
+            this.categoryRepository = categoryRepository;
+            this.productAdminRepository = productAdminRepository;
+        }
+
+        @NonNull
+        @Override
+        @SuppressWarnings("unchecked")
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            if (modelClass.isAssignableFrom(ProductManageAdminViewModel.class)) {
+                return (T) new ProductManageAdminViewModel(categoryRepository, productAdminRepository);
+            }
+            throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
+        }
     }
 }
